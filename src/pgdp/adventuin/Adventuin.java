84c321dc97f5f22ed6818b6a2ff34f62b92a2e98
Mariia Bukiaeva <bukaieva.mariia@kiu.edu.ge>
2021-12-11 16:34:41 +0400

    no message

--- src/pgdp/adventuin/Adventuin.java
+++ src/pgdp/adventuin/Adventuin.java
@@ -4,7 +4,7 @@ import pgdp.color.RgbColor;
 
 public class Adventuin {
     private final String name;
-    private final int height;
+    private final int size;
     private final RgbColor color;
     private final HatType hat;
     private final Language language;
@@ -13,8 +13,8 @@ public class Adventuin {
         return name;
     }
 
-    public int getHeight() {
-        return height;
+    public int getSize() {
+        return size;
     }
 
     public RgbColor getColor() {
@@ -29,9 +29,9 @@ public class Adventuin {
         return language;
     }
 
-    public Adventuin(String name, int height, RgbColor color, HatType hat, Language language) {
+    public Adventuin(String name, int size, RgbColor color, HatType hat, Language language) {
         this.name = name;
-        this.height = height;
+        this.size = size;
         this.color = color;
         this.hat = hat;
         this.language = language;
@@ -39,10 +39,10 @@ public class Adventuin {
 
     @Override
     public String toString() {
-        if(name == null || height == 0 || color == null || hat == null || language == null)
+        if(name == null || size == 0 || color == null || hat == null || language == null)
             return "Not allowed";
         return "name='" + name +
-                ", height=" + height +
+                ", size=" + size +
                 ", color=" + color +
                 ", hat=" + hat +
                 ", language=" + language;
